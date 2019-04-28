06520a0f9df1e960054e58455d70c6f3771ba0f3
tarurata <saycheese5963@yahoo.co.jp>
2019-04-28 20:08:05 +0900

    create data fetch(into vue data) func with scrap

--- index.php
+++ index.php
@@ -40,11 +40,6 @@
         </v-container>
         </div>
 
-    <?php
-        require('scrape.php');
-        echo $mathes;
-
-    ?>
 
         <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.js"></script>
@@ -58,7 +53,18 @@
                         var url = 'https://www.amazon.co.jp/s/ref=sr_nr_p_n_feature_nineteen_0?rh=n%3A2250738051%2Ck%3A' + encodedSearchWord + '%2Cp_n_feature_nineteen_browse-bin%3A3169286051'
                         window.open(url);
                     }
+                },
+                data: {
+                    itemData:[
+                    <?php 
+                        require('scrape.php');
+                        foreach($items as $item) {
+                            echo '{ item:"'. $item .'" },';
+                        }
+                    ?>
+                    ]
                 }
+
             })
         </script>
     </body>
--- scrape.php
+++ scrape.php
@@ -4,7 +4,14 @@ require './vendor/autoload.php';
 
 $html = file_get_contents('https://www.amazon.co.jp/s?k=%E6%8A%80%E8%A1%93%E6%9B%B8&rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&ref=sr_nr_p_n_feature_nineteen_0');
 
-$value = phpQuery::newDocument($html)->find("h2")->find("a")->find("span")->text();
+//$items = phpQuery::newDocument($html)->find("h2")->find("a")->find("span")->text();
 
-preg_match("/^(?:.*+\n?){0,3}+/", $value, $matches);
+for($i=0;$i<3;$i++){
+    $items[] = phpQuery::newDocument($html)->find("h2")->find("a")->find("span:eq($i)")->text();
+}
 
+//echo is_array($items) ? 'Array' : 'not an Array';
+
+//foreach($items as $item){
+    //echo $item . "\n";
+//}
