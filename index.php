4beabc8f27aa178b9dc18926473ba8ee0f5f1e85
tarurata <saycheese5963@gmail.com>
2024-04-01 20:49:40 -0700

    Update for latest php and vue

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <!--CDN for MaterialIcons-->
          <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons" rel="stylesheet">
          <link href="https://fonts.googleapis.com/css?family=Material+Icons" rel="stylesheet">
-         <link href="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css" rel="stylesheet">
+         <link href="https://cdn.jsdelivr.net/npm/vuetify@3.0.0/dist/vuetify.min.css" rel="stylesheet">
          <style type="text/css">
             .v-input__icon--append .v-icon {
                 <!--background-color: #FFA000;-->
@@ -37,85 +37,66 @@
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
-<img src="./logo.jpg" alt="" class="center">
-
-        <div id="app">
-
+    <img src="./logo.jpg" alt="" class="center">
+    <div id="app">
         <v-container>
-            <v-layout align-center justify-center>
-            <v-flex xs4 sm6 center>
-            <v-text-field
-                label="kindle unlimited の本を検索"
-                single-line
-                solo
-                append-icon="search" 
-                class="searchbtn"
-                @keypress.enter="kuSearch()"
-                @click:append="kuSearch()" 
-                id="searchword"
-            >
-              
-            </v-text-field>
-            </v-flex>
-            </v-layout>
+            <v-row align="center" justify="center">
+                <v-col cols="12" sm="6">
+                    <v-text-field
+                        label="kindle unlimited の本を検索"
+                        solo
+                        append-icon="search"
+                        class="searchbtn"
+                        @keypress.enter="kuSearch"
+                        @click:append="kuSearch"
+                        v-model="searchWord"
+                    ></v-text-field>
+                </v-col>
+            </v-row>
 
-
-
-        <v-flex xs12>
-        <v-layout row wrap>
-                <v-card 
-                    width="33%"
-                    class="mx-auto"
-                    v-for="item in itemdata"
-                    :href="item.link"
-                    target="new"
-                >
-                        <v-flex >
-                        <v-img 
-                            :src="item.imgsrc" alt=""
-                            max-height="200"
-                            contain
+            <v-row>
+                <v-col cols="12">
+                    <v-row>
+                        <v-card
+                            width="33%"
+                            class="mx-auto"
+                            v-for="item in itemdata"
+                            :key="item.name"
+                            :href="item.link"
+                            target="_blank"
                         >
-                        </v-img>
-                        </v-flex>
-
-                        <v-flex >
-                        <v-card-title primary-title>
-                            <div>{{item.name.substring(0, 20)}}</div>
-                        </v-card-title>
-                        </v-flex>
-
-                </v-card>
-        </v-layout>
-        </v-flex>
+                            <v-img :src="item.imgsrc" alt="" max-height="200" contain></v-img>
+                            <v-card-title>
+                                <div>{{ item.name.substring(0, 20) }}</div>
+                            </v-card-title>
+                        </v-card>
+                    </v-row>
+                </v-col>
+            </v-row>
         </v-container>
-        </div>
+    </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://unpkg.com/vue@3.x/dist/vue.global.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vuetify@3.x/dist/vuetify.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
-        <script>
-            new Vue({ 
-                el: '#app',
-                methods: {
-                    kuSearch(){
-                        var searchWord = document.getElementById('searchword').value;
-                        var encodedSearchWord = encodeURIComponent(searchWord);
-                        window.location.href = '?k=' + encodedSearchWord;
-                        
-                    }
-                },
-                data: {
-                    itemdata: <?= json_encode($items);?>
-                }
-                //mounted () {
-                    //axios
-                        //.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-                        //.then(response => (this.info = response.data.bpi))
-                //}
+    <script>
+      const { createApp } = Vue;
+      const vuetify = Vuetify.createVuetify();
 
-            })
-        </script>
-    </body>
-
+      createApp({
+        data() {
+          return {
+            searchWord: '',
+            itemdata: <?= json_encode($items); ?>,
+          };
+        },
+        methods: {
+          kuSearch() {
+            const encodedSearchWord = encodeURIComponent(this.searchWord);
+            window.location.href = '?k=' + encodedSearchWord;
+          },
+        },
+      }).use(vuetify).mount('#app');
+    </script>
+</body>
--- scrape.php
+++ scrape.php
@@ -1,17 +1,54 @@
 <?php
 
-require './vendor/autoload.php';
+// Enable error reporting for development (disable in production)
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-$k = $_REQUEST['k'];
+// User input from query parameters (sanitize this input)
+$k = filter_input(INPUT_GET, 'k', FILTER_SANITIZE_SPECIAL_CHARS);
 
+// Construct the URL
 $url = 'https://www.amazon.co.jp/s?k=' . urlencode($k) . '&rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&ref=sr_nr_p_n_feature_nineteen_0';
 
-$html = file_get_contents($url);
+// Use cURL for HTTP GET
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_ENCODING, ""); // Accepts all encodings and decodes them automatically
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');
+curl_setopt($ch, CURLOPT_ENCODING, ""); // Accepts all encodings and decodes them automatically
 
-for($i=0;$i<16;$i++){
-    $items[] = [
-                'name'=>phpQuery::newDocument($html)->find("h2")->find("a")->find("span:eq($i)")->text(),
-                'link'=>"https://www.amazon.co.jp" . phpQuery::newDocument($html)->find("h2")->find("a.a-link-normal:eq($i)")->attr('href'),
-                'imgsrc'=>phpQuery::newDocument($html)->find("img.s-image:eq($i)")->attr('src')
-               ];
+$html = curl_exec($ch);
+curl_close($ch);
+
+ //Check if HTML was returned
+if (!$html) {
+    die('Error fetching the webpage');
 }
+
+// Use DOMDocument to parse the HTML
+$dom = new DOMDocument();
+@$dom->loadHTML($html);
+$xpath = new DOMXPath($dom);
+
+// Fetch the containers for all items. Assuming each item is in a div with a specific class:
+$containers = $xpath->query("//div[@class='sg-col-20-of-24 s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small sg-col-12-of-16']");
+
+$items = [];
+
+foreach ($containers as $index => $container) {
+    // Within each container, fetch item details
+    $name = $xpath->evaluate(".//h2//a/span", $container)->item(0)->nodeValue;
+    $link = "https://www.amazon.co.jp" . $xpath->evaluate(".//h2//a[@class='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal']/@href", $container)->item(0)->nodeValue;
+    $imgSrc = $xpath->evaluate(".//img[@class='s-image']/@src", $container)->item(0)->nodeValue;
+
+    if ($name) {
+        $items[] = [
+            'name' => $name,
+            'link' => $link,
+            'imgsrc' => $imgSrc
+        ];
+    }
+}
+?>
